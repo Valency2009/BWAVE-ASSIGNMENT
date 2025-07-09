f25b7d39c983be205c90ede230a9309cf0d35193
Valency2009 <fakiyesivincent@gmail.com>
2025-07-09 19:34:28 -0400

    Basic Python Syntax

---
+++ python/#2 String Object/#3 print statement/#4 Mathematical Expression/expressions.py
@@ -0,0 +1,13 @@
+# OPERATOR - SIGN & SYMBOLS E.G +, -, *,/
+
+
+# OPERAND - The numbers 
+
+print(3 + 4)
+
+print(3.14 + 3.7)
+#concatenation
+print("Vincent" + " is a boy")
+print(int(10 + 10.7))
+#Modulos %= Remainder of a division  
+print(5 % 3)                   
