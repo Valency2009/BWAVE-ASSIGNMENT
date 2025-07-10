58f3565e98f42a0c812e36f4c612ebff53c0c243
Valency2009 <fakiyesivincent@gmail.com>
2025-07-09 20:31:03 -0400

    Add variables.py with basic print statement

---
+++ python/#2 String Object/#3 print statement/#4 Mathematical Expression/#5 Variables in Python/variables.py
@@ -0,0 +1,4 @@
+# Variable varies. They can change. They make our lives easier.
+# They are container used to store specific objects. The objects are known as data.
+firstname = 'Vincent'
+print(firstname)
